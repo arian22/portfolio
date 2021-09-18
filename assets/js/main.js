18d194ac63db84b1116296f7c426b36c7d23b967
arian <arian.mobarghei@gmail.com>
2021-09-18 17:21:30 +0430

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,7 +111,6 @@ function func_call_back_set_data(d) {
 }
 
 function ajax(call_back_func, form_data) {
-    return;
     try {
         $.ajax({
             url: "https://aratsystem.ir/arian/process.php",
